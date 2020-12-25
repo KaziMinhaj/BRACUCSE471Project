1c77d4d9833fc42015a3dfb48ab927d9cc594b23
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2020-12-25 17:55:36 +0600

    about-changes

--- menu-app/src/components/About.js
+++ menu-app/src/components/About.js
@@ -4,6 +4,8 @@ function About(props) {
     return (
         <div className="container login">
             <h1> This is a About page. </h1>
+            <h1> This is a About page. </h1>
+            <h1> This is a About page. </h1>
         </div>
     );
 }
