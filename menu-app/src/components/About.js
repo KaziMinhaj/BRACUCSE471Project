2c28cba32f0d106fdd0db5dc49ef3d228055400b
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2020-12-25 17:14:05 +0600

    demo commit

--- menu-app/src/components/About.js
+++ menu-app/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About(props) {
     return (
         <div className="container login">
-            <p>Bangladesh is a country in South Asia. It is next to the North-east Indian provincial regions of India, which converges with Southeast Asia to the east. Its full name is The People's Republic of Bangladesh. The capital and the largest city is Dhaka. Bangladesh is surrounded on three sides by the Republic of India, and Myanmar on the south-eastern corner. It is near the People's Republic of China, Bhutan, Sikkim and Nepal. The Bay of Bengal is situated to the south of the country. </p>
+            <p> This is a simple online restaurant app. It is under development. Stay with us . </p>
         </div>
     );
 }
