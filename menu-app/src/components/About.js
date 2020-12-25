94eec8bc6cd6fe02356f83f2951e379eaaa72639
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2020-12-25 17:42:08 +0600

    about page changes

--- menu-app/src/components/About.js
+++ menu-app/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About(props) {
     return (
         <div className="container login">
-            <p> This is a simple online restaurant app. It is under development. Stay with us. We will update it in near future </p>
+            <h1> This is a About page. </h1>
         </div>
     );
 }
